a1cfa03158cd06b4d54d51468efcc097bbddd9a0
VISHWARAJ <VISHWARAJ@LAPTOP-GTI4JFKO>
2025-07-04 10:56:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleapp3
+{
+    internal class program
+    {
+      public  static void Main(string[] args)
+        {
+            Console.WriteLine("enter num1");
+            int a=Convert .ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            int b=Convert .ToInt32(Console.ReadLine());
+            int sum;
+            sum = a + b;
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
